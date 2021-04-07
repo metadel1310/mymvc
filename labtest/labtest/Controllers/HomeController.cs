c62398f2fcc099d35745602f9aacfc7c2a7e279d
metadel <meetmetadel@gmail.com>
2021-04-07 15:22:29 -0700

    design the  home  page

---
+++ labtest/labtest/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace labtest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "we  are App_factory student and developing  web site if  u  want a website for  your  organaization contact us.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ labtest/labtest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="labtest.MvcApplication" Language="C#" %>
