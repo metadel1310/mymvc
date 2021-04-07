a93f60f0e708f96c83cb31930d24b040e9542c36
metadel <meetmetadel@gmail.com>
2021-04-07 09:13:04 -0700

    appppf

---
+++ grouwebsite/grouwebsite/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace grouwebsite
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
